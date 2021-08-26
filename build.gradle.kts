8e8cdb15705a015626e98f7ef23e48cde9156155
xtex <zbwxtex@qq.com>
2021-08-26 17:37:12 +0800

    fix: move mirai-core above mirai-core-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 }
 
 dependencies {
-    compileOnly("net.mamoe:mirai-core-api:2.8-M1")
     compileOnly("net.mamoe:mirai-core:2.8-M1")
+    compileOnly("net.mamoe:mirai-core-api:2.8-M1")
     compileOnly("net.mamoe:mirai-console:2.8-M1")
     implementation("com.google.code.gson:gson:2.8.8")
 }
